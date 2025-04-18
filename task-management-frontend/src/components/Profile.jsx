2a5b63a2ddeee168e6d310c2ec79b82375fc56a9
maariz0502 <aariz0502@gmail.com>
2025-04-17 23:57:40 -0500

    Added profile page

--- task-management-frontend/src/components/Profile.jsx
+++ task-management-frontend/src/components/Profile.jsx
@@ -1,9 +1,71 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react';
+import { getUser } from '../services/UserService';
 
 const Profile = () => {
-  return (
-    <div>Profile</div>
-  )
-}
+  const [user, setUser] = useState({});
 
-export default Profile
+  useEffect(() => {
+    fetchUser();
+  }, []);
+
+  async function fetchUser() {
+    try {
+      const response = await getUser();
+      setUser(response.data);
+    } catch (error) {
+      console.error("Error fetching user:", error);
+    }
+  }
+
+  return (
+    <div className='container mt-5'>
+      <div className='row'>
+        <div className='col-md-6 offset-md-3'>
+          <div className='card'>
+            <div className='card-header'>
+              <h2 className='text-center'>User Profile</h2>
+            </div>
+            <div className='card-body'>
+              <form>
+                <div className='row mb-3'>
+                  <label className='col-md-3 control-label'>Username</label>
+                  <div className='col-md-9'>
+                    <input
+                      type="text"
+                      className="form-control bg-light text-muted"
+                      value={user.username || ''}
+                      readOnly
+                      style={{
+                        cursor: "not-allowed",
+                        border: "1px solid #dee2e6",
+                        opacity: 0.95
+                      }}
+                    />
+                  </div>
+                </div>
+                <div className='row mb-3'>
+                  <label className='col-md-3 control-label'>Email</label>
+                  <div className='col-md-9'>
+                    <input
+                      type="text"
+                      className="form-control bg-light text-muted"
+                      value={user.email || ''}
+                      readOnly
+                      style={{
+                        cursor: "not-allowed",
+                        border: "1px solid #dee2e6",
+                        opacity: 0.95
+                      }}
+                    />
+                  </div>
+                </div>
+              </form>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Profile;
---
+++ task-management-frontend/src/services/UserService.js
@@ -0,0 +1,19 @@
+import axios from "axios";
+import { getToken } from "./AuthService.js";
+
+const API_URL = "http://localhost:8080/user";
+
+// Add a request interceptor
+axios.interceptors.request.use(function (config) {
+
+    config.headers['Authorization'] = getToken();
+
+    return config;
+}, function (error) {
+    // Do something with request error
+    return Promise.reject(error);
+});
+
+export const getUser = () => axios.get(API_URL)
+
+export const updateUser = () => axios.put(`${API_URL}/update-user`, user)
